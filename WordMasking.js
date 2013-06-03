9786c4f54c79950ce5f6dfe26426184623cf0f38
Danny Chen <chenxiaoqino@gmail.com>
2013-06-03 19:52:44 +0800

    Add: use touchstart and touchend event

--- WordMasking.js
+++ WordMasking.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){
+
+
+var _WM_Initialize=function(){
+	if(!window.jQuery)
+	{
+		var script = document.createElement('script');
+		script.type = "text/javascript";
+		script.src = "http://code.jquery.com/jquery-1.9.0.js";
+		document.getElementsByTagName('head')[0].appendChild(script);
+		script.onload=_WM_Initialize;
+		return;
+	}
+		
 	var color={
 		'default':'#fcc',
 		'background':'#fff',
@@ -8,13 +20,30 @@ $(document).ready(function(){
 		'background':color.default,
 		'color':color.default
 	}).hover(
-		function(){$(this).css({
-		'background':color.background,
-		'color':color.text
-		});},
-		function(){$(this).css({
-		'background':color.default,
-		'color':color.default
-		});}
-	);
-});
+		function(){
+			$(this).css({
+				'background':color.background,
+				'color':color.text
+			});
+		},
+		function(){
+			$(this).css({
+				'background':color.default,
+				'color':color.default
+			});
+		}
+	).bind("touchstart",function(){
+			$(this).css({
+				'background':color.background,
+				'color':color.text
+			});
+	}).bind("touchend",function(){
+			$(this).css({
+				'background':color.default,
+				'color':color.default
+			});	
+	});
+}
+
+window.onload=_WM_Initialize;
+$(document).ready(function(){window.onload=null;_WM_Initialize();});
