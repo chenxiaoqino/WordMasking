584e3c9f5c375f03e97d21da58741802a5486b2f
Danny Chen <chenxiaoqino@gmail.com>
2013-06-03 19:30:31 +0800

    Mod: use document.ready event to initialize.

--- WordMasking.js
+++ WordMasking.js
@@ -1,18 +1,20 @@
-var color={
-	'default':'#fcc',
-	'background':'#fff',
-	'text':'#000'
-}
-$('b').css({
-	'background':color.default,
-	'color':color.default
-}).hover(
-	function(){$(this).css({
-	'background':color.background,
-	'color':color.text
-	});},
-	function(){$(this).css({
-	'background':color.default,
-	'color':color.default
-	});}
-);
+$(document).ready(function(){
+	var color={
+		'default':'#fcc',
+		'background':'#fff',
+		'text':'#000'
+	}
+	$('b').css({
+		'background':color.default,
+		'color':color.default
+	}).hover(
+		function(){$(this).css({
+		'background':color.background,
+		'color':color.text
+		});},
+		function(){$(this).css({
+		'background':color.default,
+		'color':color.default
+		});}
+	);
+});
